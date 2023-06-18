05e6b195deda8d41cfd0678e9c5017d9b3ac0c76
lai.glorison <lai.glorison@gmail.com>
2023-06-19 03:05:36 +1000

    feat: add S-CR mapping to copilot

--- mappings.lua
+++ mappings.lua
@@ -18,8 +18,15 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["-"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
+    ["<S-Left>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" },
+    ["<S-Down>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" },
+    ["<S-Up>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" },
+    ["<S-Right>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
+  },
+  i = {
+    ["<S-CR>"] =  { function() vim.fn.feedkeys(vim.fn['copilot#Accept']("<CR>"), '') end, desc = "Horizontal Split" },
   },
   t = {
     -- setting a mapping to false will disable it
