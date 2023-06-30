1e0fecac571f2371f1b6a29ba922873eebbb3de2
Glorison <lai.glorison@gmail.com>
2023-06-30 16:33:36 +0800

    Revert "feat: copilot.vim"
    This reverts commit 89240b9a197df9450bb3db780de50bff5ca72ff7.

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,7 +2,6 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  "github/copilot.vim",
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
